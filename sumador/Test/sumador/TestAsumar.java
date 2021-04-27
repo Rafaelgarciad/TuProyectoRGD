b49d818052108123477149211ff9ff7420f48121
Rafael <rafael.gd.info@gmail.com>
2021-04-27 20:08:55 +0200

    2º comit de la tercera prueba

--- sumador/Test/sumador/TestAsumar.java
+++ sumador/Test/sumador/TestAsumar.java
@@ -7,8 +7,23 @@ import org.junit.Test;
 public class TestAsumar {
 
 	@Test
-	public void test() {
-		fail("No implementado aun");
-	}
-
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.sumativo("5");
+        assertEquals("5 = 5", resultado);
+    }
+	
+	@Test
+    public void testCifraNegativa() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.sumativo("-5");
+        assertEquals("Negativo. Siempre negativo", resultado);
+    }
+	
+	@Test
+    public void testDosCifras() {
+        ASumar sumi = new ASumar();
+        int resultado = sumi.total("13");
+        assertEquals(4, resultado);
+    }
 }
--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,27 @@
 package sumador;
 
-public class Asumar {
+public class ASumar {
+		
+	public String sumativo(String cad) {
+        String numero = cad;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        } else if (numero.substring(0, 0) == "-"){
+        	return "Negativo. Siempre negativo";
+        }
+        return "Negativo. Siempre negativo";
+        
+	}
 
+	public int total(String valor_inicial) {
+        int suma = 0;
+
+        int digito1 = Integer.parseInt(valor_inicial.substring(0, 1));
+        int digito2 = Integer.parseInt(valor_inicial.substring(1));
+        
+        suma = digito1+digito2;
+        
+        return suma;
+
+    }
 }
